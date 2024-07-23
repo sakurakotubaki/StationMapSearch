2aca0b0008b5168e20bd8aaa689466fa5059853c
Jboy422 <gitonly543@gmail.com>
2024-07-23 17:15:58 +0900

    Initial Commit

---
+++ StationMapSearch/StationMapSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StationMapSearchApp.swift
+//  StationMapSearch
+//
+//  Created by Jboy422 on 2024/07/23.
+//
+
+import SwiftUI
+
+@main
+struct StationMapSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
